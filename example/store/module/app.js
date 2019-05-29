019be5e31a1e09eb446e31ea66c0560e83d1e425
bingblog <abingblog@gmail.com>
2019-05-29 11:44:41 +0800

    update examples; add layout

---
+++ example/store/module/app.js
@@ -0,0 +1,30 @@
+/**
+ * @file app状态
+ * @author wangbing11(wangbing11@baidu.com)
+ */
+
+/* eslint-disable no-param-reassign */
+import config from '../../config';
+
+const {menuList} = config;
+export default {
+    state: {
+        menuList,
+        breadCrumbList: [],
+        tagNavList: [],
+        homeRoute: {},
+        errorList: [],
+        hasReadErrorPage: false
+    },
+    mutations: {
+        setLocal(state, lang) {
+            state.local = lang;
+        },
+        addError(state, error) {
+            state.errorList.push(error);
+        },
+        setHasReadErrorLoggerStatus(state, status = true) {
+            state.hasReadErrorPage = status;
+        }
+    }
+};
