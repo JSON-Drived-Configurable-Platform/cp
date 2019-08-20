59db369c8fc40cae0c9c0211c1298072fba9e5fa
bingblog <abingblog@gmail.com>
2019-08-20 14:07:18 +0800

    update docs

--- packages/form-generator-iview/website/page/start/index.vue
+++ packages/form-generator-iview/website/page/start/index.vue
@@ -8,9 +8,9 @@
                 h2
             />
             <ul>
-                <li>高质量、功能强大</li>
-                <li>细致、漂亮的 UI</li>
-                <li>事无巨细的文档</li>
+                <li>可配置、高效率</li>
+                <li>可复用、易维护</li>
+                <li>文档详细，查询方便</li>
             </ul>
 
             <inAnchor
@@ -61,10 +61,58 @@
                 <li>支持 iView 3.0</li>
             </ul>
 
+            <inAnchor
+                title="目标用户"
+                h2
+            />
+            <p>已经或者打算使用iView组件库来开发中台的<strong>前端同学</strong>。</p>
+            <p>特别项目中有很多的表单场景，直接使用模板开发会导致开发量增加、维护起来很困难。</p>
+            <blockquote>
+                如果你是从头开始开发一个中台，可以先看看<a href="https://github.com/BingBlog/cp">CP</a>。这个项目中汇总了很多中台中使用到的工具和模板。有可能会帮助到你。
+            </blockquote>
+            <blockquote>
+                如果你是一个不了解前端技术的<strong>后端同学</strong>，可以考虑使用<a href="https://github.com/baidu/amis" target="_blank">amis配置工具</a>。
+                这个工具可以让你在没有前端同学支持的情况下配置出完整的平台，功能非常强大。
+            </blockquote>
+
+            <inAnchor
+                title="关于文档"
+                h2
+            />
+            <p>文档的作用主要有一下几点：</p>
+            <ul>
+                <li>1. 帮助您了解如何使用这个工具；</li>
+                <li>2. 提供各个组件的配置速查表；这些表对于<strong>后端同学</strong>了解如何更改配置很有帮助。</li>
+            </ul>
+
+            <!-- TODO -->
+            <inAnchor
+                title="场景限制"
+                h2
+            />
+            <p>做成可配置的，其实是用灵活性去换效率。使用时，需要仔细考虑是否使用可配置的方式。目前的功能会在处理一下几个场景时遇到问题：</p>
+            <ul>
+                <li>交互中，多个表单项之间有关联逻辑。特别是展现关联、校验关联的场景；</li>
+            </ul>
+
+            <inAnchor
+                title="与iView的关系"
+                h2
+            />
+            <p>工具中的大部分组件都基于iView组件开发。所以在设计上，很多命名都尽量保持与iView一致，包含组件名称、组件属性名称、组件方法名称等。</p>
+            <p>这个工具仅仅是对iView做了一些封装。目的是为了让使用iView的同时，也可以通过配置生成表单。希望对iView社区有一定帮助。</p>
+            <p>选用iView是因为iView社区的活跃度很高，组件库和文档的质量也很好。</p>
+
             <inAnchor title="相关链接" h2 />
             <ul>
                 <li>
                     <a href="https://iviewui.com/" target="_blank">iView官方文档</a>
+                </li>
+                <li>
+                    <a href="https://github.com/BingBlog/cp" target="_blank">A configurable platform frontend ui solution based on iView UI.</a>
+                </li>
+                <li>
+                    <a href="https://github.com/baidu/amis" target="_blank">baidu出品的amis配置工具</a>
                 </li>
             </ul>
 
