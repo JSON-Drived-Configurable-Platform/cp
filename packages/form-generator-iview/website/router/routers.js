9c843490cb5ed53d23d5e7b4ccae8217592630ec
bingblog <abingblog@gmail.com>
2019-08-21 14:12:16 +0800

    update routers for splitting load

--- packages/form-generator-iview/website/router/routers.js
+++ packages/form-generator-iview/website/router/routers.js
@@ -5,18 +5,20 @@
 
 import Main from '../components/main';
 
-import home from '../page/home';
-import start from '../page/start';
+// import home from '../page/home';
+// import start from '../page/start';
+// import practice from '../page/practice';
 
-import practice from '../page/practice';
-import formGeneratorExample from '../page/examples/form-generator-example';
-import fieldGeneratorExample from '../page/examples/field-generator-example';
-import FormGenerator from '../page/doc/FormGenerator';
-import FieldGenerator from '../page/doc/FieldGenerator';
-import Input from '../page/doc/Input';
-import Radio from '../page/doc/Radio';
-import Checkbox from '../page/doc/Checkbox';
-import Switch from '../page/doc/Switch';
+// import FormGenerator from '../page/doc/FormGenerator';
+// import FieldGenerator from '../page/doc/FieldGenerator';
+// import Input from '../page/doc/Input';
+// import Radio from '../page/doc/Radio';
+// import Checkbox from '../page/doc/Checkbox';
+// import Switch from '../page/doc/Switch';
+
+// import formGeneratorExample from '../page/examples/form-generator-example';
+// import fieldGeneratorExample from '../page/examples/field-generator-example';
+
 export default [
     {
         path: '/',
@@ -27,17 +29,17 @@ export default [
             {
                 path: '/home',
                 name: 'home',
-                component: home
+                component: (resolve) => require(['../page/home'], resolve)
             },
             {
                 path: '/start',
                 name: 'start',
-                component: start
+                component: (resolve) => require(['../page/start'], resolve)
             },
             {
                 path: '/practice',
                 name: 'practice',
-                component: practice
+                component: (resolve) => require(['../page/practice'], resolve)
             },
             {
                 path: '/doc',
@@ -47,42 +49,42 @@ export default [
             {
                 path: '/doc/FormGenerator',
                 name: 'doc-FormGenerator',
-                component: FormGenerator
+                component: (resolve) => require(['../page/doc/FormGenerator'], resolve)
             },
             {
                 path: '/doc/FieldGenerator',
                 name: 'doc-FieldGenerator',
-                component: FieldGenerator
+                component: (resolve) => require(['../page/doc/FieldGenerator'], resolve)
             },
             {
                 path: '/doc/Input',
                 name: 'doc-Input',
-                component: Input
+                component: (resolve) => require(['../page/doc/Input'], resolve)
             },
             {
                 path: '/doc/Radio',
                 name: 'doc-Radio',
-                component: Radio
+                component: (resolve) => require(['../page/doc/Radio'], resolve)
             },
             {
                 path: '/doc/Checkbox',
                 name: 'doc-Checkbox',
-                component: Checkbox
+                component: (resolve) => require(['../page/doc/Checkbox'], resolve)
             },
             {
                 path: '/doc/Switch',
                 name: 'doc-Switch',
-                component: Switch
+                component: (resolve) => require(['../page/doc/Switch'], resolve)
             },
             {
                 path: '/form-generator-example',
                 name: 'data',
-                component: formGeneratorExample
+                component: (resolve) => require(['../page/examples/form-generator-example'], resolve)
             },
             {
                 path: '/field-generator-example',
                 name: 'data',
-                component: fieldGeneratorExample
+                component: (resolve) => require(['../page/examples/field-generator-example'], resolve)
             },
         ]
     }
