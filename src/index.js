eab2892c39d97cff307948e1379adec62b451c18
bingblog <abingblog@gmail.com>
2019-08-22 13:57:33 +0800

    add Vue.prototype.FormGeneratorInstallOptions

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import FormGenerator from './formGenerator';
 import FieldGenerator from './fieldGenerator';
 
-const install = function(Vue) {
+const install = function(Vue, installOptions = null) {
     if (install.installed) return;
+    if (installOptions) {
+        Vue.prototype.FormGeneratorInstallOptions = installOptions;
+    }
     Vue.component('FormGenerator', FormGenerator);
     Vue.component('FieldGenerator', FieldGenerator);
 };
