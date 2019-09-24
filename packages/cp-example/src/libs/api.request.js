5d0d0f7a12997417193d8549b6abcda0c58d8e09
wangbing11 <wangbing11@baidu.com>
2019-09-24 18:15:22 +0800

    update docs

--- packages/cp-example/src/libs/api.request.js
+++ packages/cp-example/src/libs/api.request.js
@@ -5,10 +5,7 @@
 
 import HttpRequest from "@/libs/axios";
 import config from "@/config";
-const baseUrl =
-  process.env.NODE_ENV === "development"
-    ? config.baseUrl.dev
-    : config.baseUrl.pro;
+const baseUrl = config.baseUrl;
 
 const axios = new HttpRequest(baseUrl);
 export default axios;
