890b56d19c4b4ea7838de51f4e81b258d228f5df
Tobin1990 <570432462@qq.com>
2019-11-29 11:28:13 +0800

    fieldsubmit

--- src/fields/fieldSubmit.vue
+++ src/fields/fieldSubmit.vue
@@ -5,16 +5,44 @@
     >{{ field.text }}</Button>
 </template>
 <script>
+import axios from '../utils/http';
 export default {
+    inject: ['form'],
     props: {
         field: {
             type: Object,
             required: true
         },
+        apiBase: {
+            type: String,
+            default: ''
+        }
     },
     methods: {
         handleClick() {
-            this.$emit('on-submit-click');
+            switch (this.field.action.type) {
+                case 'ajax':
+                    this.submitAjaxAction();
+                    break;
+                case 'event':
+                    this.$emit('on-submit-click', {
+                        name: this.field.action.name
+                    });
+            }
+        },
+        submitAjaxAction() {
+            let apiBase = this.apiBase || '';
+            let formModel = this.form.model;
+            let url = apiBase + this.field.action.api;
+            axios.request({
+                url,
+                method: 'get',
+                params: formModel
+            }).then(() => {
+                this.$emit('on-submit-click', {
+                    name: this.field.action.name
+                });
+            });
         }
     }
 };
