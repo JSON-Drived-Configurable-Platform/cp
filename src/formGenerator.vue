68c9206fcfdfacadb96f8378888179a59ae0fac4
glsnb <guoyu5212@163.com>
2019-12-11 20:48:09 +0800

    update checkboxCard (#37)
    * Set theme jekyll-theme-cayman
    
    * 1.0CheckboxCard doc
    
    * checkboxCard文档
    
    * 构建
    
    * 完善checkboxCard文档
    
    * 添加checkboxCard配置和事件
    
    * 添加checkboxCard配置和事件
    
    * 第一版checkboxCard
    
    * checkboxCard第一版
    
    * 第一版checkboxCard
    
    * 完善文档
    
    * checkboxCard 分页
    
    * checkboxCard 分页
    
    * checkboxCardapi
    
    * checkboxCardapi
    
    * checkboxCard补充文档
    
    * 注释规范
    
    * 注释规范
    
    * uplate 可编辑
    
    * update Doc
    
    * update Doc
    
    * 暴露到form组件
    
    * 添加loading
    
    * 处理index
    
    * update checkboxCard

--- src/formGenerator.vue
+++ src/formGenerator.vue
@@ -44,6 +44,7 @@
                     @on-submit="handleSubmit($event)"
                     @on-reset="handleReset"
                     @on-button-event="handleButtonEvent($event)"
+                    @on-checkboxCard-click="handelCheckboxCardClick"
                 />
             </div>
 
@@ -363,6 +364,10 @@ export default {
 
         handleButtonEvent($event) {
             this.$emit('on-button-event', $event);
+        },
+
+        handelCheckboxCardClick(value) {
+            this.$emit('on-checkboxCard-click', value);
         },
 
         handleExtraBtnClick() {
